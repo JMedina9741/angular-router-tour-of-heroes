934236fee61981d7f324280e8409156640b81bc0
JMedina9741 <147419480+JMedina9741@users.noreply.github.com>
2023-11-18 03:07:40 +0100

    first-commit

---
+++ src/app/crisis-center/crises/crisis-center.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CrisisCenterHomeComponent } from '../crisis-center-home/crisis-center-home.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { CrisisCenterComponent } from '../crisis-center/crisis-center.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
+
+import { canDeactivateGuard } from '../../can-deactivate.guard';
+import { crisisDetailResolver } from '../crisis-detail.resolver';
+import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    CrisisCenterRoutingModule
+  ],
+  declarations: [
+    CrisisCenterComponent,
+    CrisisListComponent,
+    CrisisCenterHomeComponent,
+    CrisisDetailComponent
+  ]
+})
+export class CrisisCenterModule {}
+
+
+
+
+
+
+
+
+
+
+
+
